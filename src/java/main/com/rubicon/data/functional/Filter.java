5800c1f837ef7522e425ace8f5c16d4e1261b435
stingleff <stingleff@Sam-Tingleffs-MacBook-Pro.local>
2010-04-01 13:44:58 -0700

    added an evaluate() method

--- src/java/main/com/rubicon/data/functional/Filter.java
+++ src/java/main/com/rubicon/data/functional/Filter.java
@@ -8,12 +8,7 @@ public class Filter<E> {
 	public List<E> filter(Iterable<E> values, Predicate<E>... predicates) {
 		List<E> result = new LinkedList<E>();
 		for (E e : values) {
-			boolean accept = true;
-			for (Predicate<E> predicate : predicates)
-				if (!predicate.evaluate(e)) {
-					accept = false;
-					break;
-				}
+			boolean accept = evaluate(e, predicates);
 			if (accept)
 				result.add(e);
 		}
@@ -23,15 +18,30 @@ public class Filter<E> {
 	public List<E> filter(Iterable<E> values, List<Predicate<E>> predicates) {
 		List<E> result = new LinkedList<E>();
 		for (E e : values) {
-			boolean accept = true;
-			for (Predicate<E> predicate : predicates)
-				if (!predicate.evaluate(e)) {
-					accept = false;
-					break;
-				}
+			boolean accept = evaluate(e, predicates);
 			if (accept)
 				result.add(e);
 		}
 		return result;
+	}
+
+	public boolean evaluate(E e, Predicate<E>... predicates) {
+		boolean accept = true;
+		for (Predicate<E> predicate : predicates)
+			if (!predicate.evaluate(e)) {
+				accept = false;
+				break;
+			}
+		return accept;
+	}
+
+	public boolean evaluate(E e, List<Predicate<E>> predicates) {
+		boolean accept = true;
+		for (Predicate<E> predicate : predicates)
+			if (!predicate.evaluate(e)) {
+				accept = false;
+				break;
+			}
+		return accept;
 	}
 }
