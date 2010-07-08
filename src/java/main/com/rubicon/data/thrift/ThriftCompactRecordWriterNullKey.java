46769b953e3b14a4a9bfa4dcbf71d15e364c7f89
stingleff <stingleff@Sam-Tingleffs-MacBook-Pro.local>
2010-07-07 20:45:22 -0700

    record writer for null key format

---
+++ src/java/main/com/rubicon/data/thrift/ThriftCompactRecordWriterNullKey.java
@@ -0,0 +1,34 @@
+package com.rubicon.data.thrift;
+
+import java.io.IOException;
+
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.mapreduce.RecordWriter;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.thrift.TBase;
+
+import com.rubicon.data.thrift.types.TNull;
+
+public class ThriftCompactRecordWriterNullKey<V extends TBase> extends
+		RecordWriter<TNull, V> {
+	private ThriftCompactSerializer<V> serializer = new ThriftCompactSerializer<V>();
+
+	private FSDataOutputStream out;
+
+	public ThriftCompactRecordWriterNullKey(FSDataOutputStream out)
+			throws IOException {
+		this.out = out;
+		this.serializer.open(out);
+	}
+
+	public void write(TNull key, V value) throws IOException {
+		this.serializer.serialize(value);
+	}
+
+	public void close(TaskAttemptContext context) throws IOException,
+			InterruptedException {
+		this.serializer.close();
+		this.out.close();
+	}
+
+}
