5f970f045d0976e270747af25a2642400f12d38c
stingleff <stingleff@Sam-Tingleffs-MacBook-Pro.local>
2011-11-04 13:46:22 -0700

    removed debugging println

--- src/java/main/com/rubicon/data/thrift/ReflectionHelper.java
+++ src/java/main/com/rubicon/data/thrift/ReflectionHelper.java
@@ -10,9 +10,6 @@ public class ReflectionHelper {
 	public static <T> Class<? extends T> getClassFromConfiguration(
 			Configuration conf, String param) {
 		try {
-			System.err.println("value class conf: "
-					+ conf.get(Constants.VALUE_CLASS));
-			System.err.println(conf);
 			return (Class<T>) Class.forName(conf.get(param));
 		} catch (ClassNotFoundException e) {
 			throw new RuntimeException(e);
